7edbe27ef420587270e2bf832992ebc1f2672f71
vkluchysvet <svetlanatylerjoshya@gmail.com>
2021-11-11 13:58:03 +0300

    Добавьте файлы проекта.

---
+++ Пример 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Пример_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Объявляем два массива
+            int[] myArr = new int[5];
+            int[,] myTwoArr = new int[5, 6];
+            Random ran = new Random();
+            //Инициализируем массивы
+            for (int i = 1; i <= 5; i++)
+            {
+                myArr[i - 1] = ran.Next(1, 20);
+                for (int j = 1; j <= 6; j++)
+                    myTwoArr[i - 1, j - 1] = ran.Next(10, 30);
+            }
+            //Ввычисляем квадрат каждого элемента одномерного массива
+            foreach (int f in myArr)
+                Console.WriteLine("{0} в квадрате равно {1}", f, f * f);
+            Console.WriteLine();
+            //Вычислим сумма элементов многомерного массива
+            int sum = 0;
+            foreach (int f2 in myTwoArr)
+                sum += f2;
+            Console.WriteLine("Сумма 2d массива: {0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ пример 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пример_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //объявляем два массива
+            int[] myArr = new int[5];
+            int[,] myTwoArr = new int[5, 6];
+            int sum = 0;
+            Random ran = new Random();
+            //Инициализируем массивы
+            for(int i=1;i<=5;i++)
+            {
+                myArr[i - 1] = ran.Next(1, 20);
+                for (int j = 1; j <= 6; j++)
+                    myTwoArr[i - 1, j - 1] = ran.Next(1, 30);
+                //вычисляем квадрат каждого элемента одномерного массива
+                foreach (int fVar in myArr)
+                    Console.WriteLine("{0} в квадрате равно {1}", fVar, fVar * fVar);
+                Console.WriteLine();
+                //Вычислим сумму элементов многомерного массива
+                foreach (int fTwoVar in myTwoArr)
+                    sum += fTwoVar;
+                Console.WriteLine("Сумма элементов многомерного массива: {0}", sum);
+                Console.ReadLine();
+            }
+        }
+    }
+}
